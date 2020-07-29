100532713193ea1152c0874c22debbd5f765316e
Manvi Sethi <55999896+ManviSethi@users.noreply.github.com>
2020-07-29 15:25:48 +0530

    Add files via upload

---
+++ star_red.py
@@ -0,0 +1,13 @@
+import turtle
+window= turtle.Screen()
+obj =turtle.Turtle()
+obj.shape("triangle")
+turtle.pencolor("RED")
+def square ():
+    turtle.forward (100)
+    turtle.right(90)
+for i in range (1,20):
+    square()
+    turtle.right(10)
+    
+window.exitonclick()
